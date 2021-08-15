bfe886891cbbf20de3ea3933938dd38dc7b5f36a
MartMbithi <martdevelopers254@gmail.com>
2021-08-15 15:51:24 +0300

    Implement Category Ailment Module

--- views/admin_category_ailments.php
+++ views/admin_category_ailments.php
@@ -83,11 +83,11 @@ require_once('../partials/head.php');
             <div class="content">
                 <!-- Navigation -->
                 <?php require_once('../partials/top_nav.php'); ?>
-                <h2 class="text-center">Pet Categories</h2>
+                <h2 class="text-center">Pet Categories Ailments</h2>
                 <hr>
                 <div class="text-right">
                     <a href="#add_modal" class="btn btn-primary" data-toggle="modal">
-                        Add Category
+                        Add Category Ailment
                     </a>
                 </div>
                 <br>
@@ -106,14 +106,36 @@ require_once('../partials/head.php');
                                 <form method="post" enctype="multipart/form-data" role="form">
                                     <div class="card-body">
                                         <div class="row">
-                                            <div class="form-group col-md-12">
-                                                <label for="">Category Name</label>
-                                                <input type="text" required name="category_name" class="form-control">
+                                            <div class="form-group col-md-6">
+                                                <label for="">Pet Category Name</label>
+                                                <select type="text" required name="category_ailment_category_id" class="form-control">
+                                                    <?php
+                                                    $ret = "SELECT * FROM pets_categories";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    while ($category = $res->fetch_object()) {
+                                                    ?>
+                                                        <option value="<?php echo $category->category_id; ?>"><?php echo $category->category_name; ?></option>
+                                                    <?php
+                                                    } ?>
+                                                </select>
                                             </div>
 
-                                            <div class="form-group col-md-12">
-                                                <label for="">Category Details</label>
-                                                <textarea type="text" required name="category_desc" rows="5" class="form-control"></textarea>
+                                            <div class="form-group col-md-6">
+                                                <label for="">Ailment </label>
+                                                <select type="text" required name="category_ailment_ailment_id" class="form-control">
+                                                    <?php
+                                                    $ret = "SELECT * FROM ailment";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    while ($ailment = $res->fetch_object()) {
+                                                    ?>
+                                                        <option value="<?php echo $ailment->ailment_id; ?>"><?php echo $ailment->ailment_name; ?></option>
+                                                    <?php
+                                                    } ?>
+                                                </select>
                                             </div>
                                         </div>
                                     </div>
@@ -140,66 +162,32 @@ require_once('../partials/head.php');
                             <table class="table">
                                 <thead>
                                     <tr>
-                                        <th>Name</th>
-                                        <th>Details</th>
+                                        <th>Pet Category Name</th>
+                                        <th>Ailment Name</th>
+                                        <th>Ailment Description</th>
+                                        <th>Ailment Symptoms</th>
                                         <th>Manage</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php
-                                    $ret = "SELECT * FROM pets_categories";
+                                    $ret = "SELECT * FROM category_ailments ca 
+                                    INNER JOIN pets_categories pc ON pc.category_id = ca.category_ailment_category_id
+                                    INNER JOIN ailment a ON a.ailment_id = ca.category_ailment_ailment_id";
                                     $stmt = $mysqli->prepare($ret);
                                     $stmt->execute(); //ok
                                     $res = $stmt->get_result();
-                                    while ($category = $res->fetch_object()) {
+                                    while ($category_ailment = $res->fetch_object()) {
                                     ?>
                                         <tr>
-                                            <th><?php echo $category->category_name; ?></th>
-                                            <td><?php echo $category->category_desc; ?></td>
+                                            <th><?php echo $category_ailment->category_name; ?></th>
+                                            <td><?php echo $category_ailment->ailment_name; ?></td>
+                                            <td><?php echo $category_ailment->ailment_desc; ?></td>
+                                            <td><?php echo $category_ailment->ailment_signs; ?></td>
                                             <td>
-                                                <a class="badge badge-primary" data-toggle="modal" href="#update-<?php echo $category->category_id; ?>">Update</a>
-                                                <!-- Update Modal -->
-                                                <div class="modal fade" id="update-<?php echo $category->category_id; ?>">
-                                                    <div class="modal-dialog  modal-lg">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h4 class="modal-title">Fill All Fields </h4>
-                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                    <span aria-hidden="true">&times;</span>
-                                                                </button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <!-- Add Module Form -->
-                                                                <form method="post" enctype="multipart/form-data" role="form">
-                                                                    <div class="card-body">
-                                                                        <div class="row">
-                                                                            <div class="form-group col-md-12">
-                                                                                <label for="">Category Name</label>
-                                                                                <input type="text" value="<?php echo $category->category_name; ?>" required name="category_name" class="form-control">
-                                                                                <input type="hidden" value="<?php echo $category->category_id; ?>" required name="category_id" class="form-control">
-                                                                            </div>
-
-                                                                            <div class="form-group col-md-12">
-                                                                                <label for="">Category Details</label>
-                                                                                <textarea type="text" required name="category_desc" rows="5" class="form-control"><?php echo $category->category_desc; ?></textarea>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="text-right">
-                                                                        <button type="submit" name="update_category" class="btn btn-primary">Update Category</button>
-                                                                    </div>
-                                                                </form>
-                                                                <!-- End Module Form -->
-                                                            </div>
-
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <!-- End Modal -->
-
-                                                <a class="badge badge-danger" data-toggle="modal" href="#delete-<?php echo $category->category_id; ?>">Delete Category</a>
+                                                <a class="badge badge-danger" data-toggle="modal" href="#delete-<?php echo $category_ailment->category_ailment_id; ?>">Delete Category</a>
                                                 <!-- Delete Modal -->
-                                                <div class="modal fade" id="delete-<?php echo $category->category_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal fade" id="delete-<?php echo $category_ailment->category_ailment_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                     <div class="modal-dialog modal-dialog-centered" role="document">
                                                         <div class="modal-content">
                                                             <div class="modal-header">
@@ -209,10 +197,10 @@ require_once('../partials/head.php');
                                                                 </button>
                                                             </div>
                                                             <div class="modal-body text-center text-danger">
-                                                                <h4>Delete <?php echo $category->category_name; ?></h4>
+                                                                <h4>Delete Category Ailment?</h4>
                                                                 <br>
                                                                 <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
-                                                                <a href="admin_pets_categories?delete=<?php echo $category->category_id; ?>" class="text-center btn btn-danger"> Delete </a>
+                                                                <a href="admin_category_ailments?delete=<?php echo $category_ailment->category_ailment_id; ?>" class="text-center btn btn-danger"> Delete </a>
                                                             </div>
                                                         </div>
                                                     </div>
