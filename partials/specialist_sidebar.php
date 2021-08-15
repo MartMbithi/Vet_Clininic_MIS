8c7744ad3854907bd849a226fb4a65d8eb8956da
MartMbithi <martdevelopers254@gmail.com>
2021-08-15 19:33:26 +0300

    Implement Specialist Dashboard

---
+++ partials/specialist_sidebar.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-light navbar-glass navbar-top sticky-kit navbar-expand-lg" style="display:none;">
+    <button class="btn navbar-toggler-humburger-icon navbar-toggler mr-1 mr-sm-3" type="button" data-toggle="collapse" data-target="#navbarStandard" aria-controls="navbarStandard" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
+    <a class="navbar-brand mr-1 mr-sm-3" href="specialist_dashboard">
+        <div class="d-flex align-items-center"><img class="mr-2" src="../public/uploads/system_data/pet.svg" alt="" width="40" /><span class="text-sans-serif">Pet Health MIS</span></div>
+    </a>
+    <div class="collapse navbar-collapse" id="navbarStandard">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown dropdown-on-hover"><a class="nav-link dropdown-toggle" id="navbarDropdownHome" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Home</a>
+                <div class="dropdown-menu dropdown-menu-card" aria-labelledby="navbarDropdownHome">
+                    <div class="bg-white rounded-soft py-2"><a class="dropdown-item" href="specialist_dashboard">Dashboard</a><a class="dropdown-item" href="home/dashboard-alt.html">Dashboard alt</a><a class="dropdown-item" href="home/feed.html">Feed</a><a class="dropdown-item" href="home/landing.html">Landing</a></div>
+                </div>
+            </li>
+            <li class="nav-item dropdown dropdown-on-hover"><a class="nav-link dropdown-toggle" id="navbarDropdownPages" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+                <div class="dropdown-menu dropdown-menu-card" aria-labelledby="navbarDropdownPages">
+                    <div class="card navbar-card-pages shadow-none">
+                        <div class="card-body scrollbar perfect-scrollbar max-h-dropdown"><img class="position-absolute b-0 r-0" src="assets/img/illustrations/authentication-corner.png" width="130" alt="" />
+                            <div class="row">
+                                <div class="col-6 col-md-4">
+                                    <div class="nav flex-column"><a class="nav-link py-1 text-700 font-weight-medium" href="pages/activity.html">Activity</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/associations.html">Associations</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/billing.html">Billing</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/customer-details.html">Customer details</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/event-detail.html">Event detail</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/event-create.html">Event create</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/events.html">Events</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/faq.html">Faq</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/invoice.html">Invoice</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/invite-people.html">Invite people</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/notifications.html">Notifications</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/people.html">People</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/pricing.html">Pricing</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/pricing-alt.html">Pricing alt</a></div>
+                                </div>
+                                <div class="col-6 col-md-4">
+                                    <div class="nav flex-column"><a class="nav-link py-1 text-700 font-weight-medium" href="pages/profile.html">Profile</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/settings.html">Settings</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/starter.html">Starter</a><a class="nav-link py-1 text-700 font-weight-medium" href="calendar.html">Calendar</a><a class="nav-link py-1 text-700 font-weight-medium" href="chat.html">Chat</a><a class="nav-link py-1 text-700 font-weight-medium" href="kanban.html">Kanban</a><a class="nav-link py-1 text-700 font-weight-medium" href="widgets.html">Widgets</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/errors/404.html">404</a><a class="nav-link py-1 text-700 font-weight-medium" href="pages/errors/500.html">500</a>
+                                        <div class="nav-link py-1 text-900 font-weight-bold mt-3">Emails</div><a class="nav-link py-1 text-700 font-weight-medium" href="email/inbox.html">Inbox</a><a class="nav-link py-1 text-700 font-weight-medium" href="email/email-detail.html">Email detail</a><a class="nav-link py-1 text-700 font-weight-medium" href="email/compose.html">Compose</a>
+                                    </div>
+                                </div>
+                                <div class="col-6 col-md-4">
+                                    <div class="nav flex-column">
+                                        <div class="nav-link py-1 text-900 font-weight-bold">E-commerce</div><a class="nav-link py-1 text-700 font-weight-medium" href="e-commerce/product-list.html">Product list</a><a class="nav-link py-1 text-700 font-weight-medium" href="e-commerce/product-grid.html">Product grid</a><a class="nav-link py-1 text-700 font-weight-medium" href="e-commerce/product-details.html">Product details</a><a class="nav-link py-1 text-700 font-weight-medium" href="e-commerce/orders.html">Orders</a><a class="nav-link py-1 text-700 font-weight-medium" href="e-commerce/order-details.html">Order details</a><a class="nav-link py-1 text-700 font-weight-medium" href="e-commerce/customers.html">Customers</a><a class="nav-link py-1 text-700 font-weight-medium" href="e-commerce/shopping-cart.html">Shopping cart</a><a class="nav-link py-1 text-700 font-weight-medium" href="e-commerce/checkout.html">Checkout</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+        </ul>
+    </div>
+    <ul class="navbar-nav navbar-nav-icons ml-auto flex-row align-items-center">
+
+    </ul>
+</nav>
---
+++ partials/specialist_topbar.php
@@ -0,0 +1,118 @@
+<nav class="navbar navbar-vertical navbar-expand-xl navbar-light" style="display:none;">
+    <script>
+        var navbarStyle = localStorage.getItem("navbarStyle");
+        if (navbarStyle) {
+            document.querySelector('.navbar-vertical').className += ' navbar-' + navbarStyle;
+        }
+    </script>
+    <div class="d-flex align-items-center">
+        <a class="navbar-brand" href="admin_dashboard">
+            <div class="d-flex align-items-center py-3"><img class="mr-2" src="../public/uploads/system_data/pet.svg" alt="" width="40" /><span class="text-sans-serif">Pet MIS</span></div>
+        </a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarVerticalCollapse">
+        <div class="navbar-vertical-content perfect-scrollbar scrollbar">
+            <ul class="navbar-nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="specialist_dashboard">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-home"> </span>
+                            </span>
+                            <span class="nav-link-text"> Home</span>
+                        </div>
+                    </a>
+                </li>
+                <!--  <li class="nav-item">
+                    <a class="nav-link" href="admin_specialists">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-user-nurse"> </span>
+                            </span>
+                            <span class="nav-link-text"> Specialists</span>
+                        </div>
+                    </a>
+                </li> -->
+                <li class="nav-item">
+                    <a class="nav-link" href="specialist_customers">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-users"> </span>
+                            </span>
+                            <span class="nav-link-text"> Customers</span>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="specialist_pets_categories">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-paw"> </span>
+                            </span>
+                            <span class="nav-link-text"> Pets Categories</span>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="specialist_pets">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-cat"> </span>
+                            </span>
+                            <span class="nav-link-text"> Pets</span>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link dropdown-indicator" href="#pages" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="pages">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-viruses"></span>
+                            </span>
+                            <span class="nav-link-text">Ailments</span>
+                        </div>
+                    </a>
+                    <ul class="nav collapse" id="pages" data-parent="#navbarVerticalCollapse">
+                        <li class="nav-item"><a class="nav-link" href="specialist_ailment">Ailment </a></li>
+                        <li class="nav-item"><a class="nav-link" href="specialist_category_ailments">Category Ailments</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="specialist_clinic_vists">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-clinic-medical"> </span>
+                            </span>
+                            <span class="nav-link-text"> Clinic Visits</span>
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link dropdown-indicator" href="#page" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="pages">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-copy"></span>
+                            </span>
+                            <span class="nav-link-text">Reports</span>
+                        </div>
+                    </a>
+                    <ul class="nav collapse" id="page" data-parent="#navbarVerticalCollapse">
+                        <li class="nav-item"><a class="nav-link" href="specialist_reports_customer_pets">Customer Pets </a></li>
+                        <li class="nav-item"><a class="nav-link" href="specialist_reports_clinic_vists">Clinic Visits</a></li>
+                        <li class="nav-item"><a class="nav-link" href="specialist_reports_customers">Customers</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="specialis_logout">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-power-off"> </span>
+                            </span>
+                            <span class="nav-link-text"> Log Out</span>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
